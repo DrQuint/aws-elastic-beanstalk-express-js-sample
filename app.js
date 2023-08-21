0a55a2de810333529a8dc4666519e64e00535102
DrQuint <emailfalsonumero4@gmail.com>
2023-08-21 20:20:35 +0800

    aa

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Whatever Something Else'));
+app.get('/', (req, res) => res.send('Something else'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
